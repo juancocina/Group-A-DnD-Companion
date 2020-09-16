fe89cfe2fa38bc61b30d3db01221f77df0e49f58
Benjamin Fuller <benisfull@gmail.com>
2020-09-15 21:37:56 -0700

    Added directory and moved Dice Roller Module into directory

---
+++ DiceRoller/main.py
@@ -0,0 +1,39 @@
+# Group A
+# Stephen Lee
+# Dice rolling module
+
+import random
+
+def numDie():
+    num = input("How many die will you roll? ")
+    return num
+
+def sideDie():
+    side = input("How many sides does your dice/die have? ")
+    return side
+
+def modifier():
+    mod = input("Enter modifier value: ")
+    return mod
+
+def typeRoll(num, side):
+    type = "{}d{}".format(num, side)
+    return type
+
+def rollDice(sides):
+    result = random.randint(1, sides)
+    return result
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    total = 0
+    num = int(numDie())
+    side = int(sideDie())
+    mod = int(modifier())
+    type = typeRoll(num, side)
+    for x in range(num):
+        roll = rollDice(side)
+        print("Die #{}: {}".format(x+1, roll))
+        total += roll
+    total += mod
+    print("{}: {}".format(type, total))
