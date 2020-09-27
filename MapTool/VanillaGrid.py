b77aa8d5ecb8a207e68a93b850e9e089c90e31de
Benjamin Fuller <benisfull@gmail.com>
2020-09-26 20:25:26 -0700

    new options for line colors

--- MapTool/VanillaGrid.py
+++ MapTool/VanillaGrid.py
@@ -9,28 +9,21 @@ try:
 except ImportError:
     import Image
 
-# Open image file
 image = Image.open('ForestMap.png')
 my_dpi = 200
 
-# Set up figure
 fig = plt.figure(figsize=(float(image.size[0]) / my_dpi, float(image.size[1]) / my_dpi), dpi=my_dpi)
 ax = fig.add_subplot(111)
 
-# Remove whitespace from around the image
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
-# Set the gridding interval using major tick
 myInterval = 133
 loc = plticker.MultipleLocator(base=myInterval)
 ax.xaxis.set_major_locator(loc)
 ax.yaxis.set_major_locator(loc)
 
-# Add the grid
 ax.grid(which='major', axis='both', linestyle='-', color='w')
 
-# Add the image underneath grid
 ax.imshow(image)
 
-# Save and create as new image, keeping integrity of old image
 fig.savefig('ForestMapGrid.png', dpi=my_dpi)
