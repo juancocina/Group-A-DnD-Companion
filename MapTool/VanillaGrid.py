e2e30a597d691d809a2fa966f3e8745f08e54a13
Benjamin Fuller <benisfull@gmail.com>
2020-09-26 20:31:12 -0700

    added colors for lines v2

--- MapTool/VanillaGrid.py
+++ MapTool/VanillaGrid.py
@@ -9,21 +9,21 @@ try:
 except ImportError:
     import Image
 
-image = Image.open('ForestMap.png')
+background = Image.open('ForestMap.png')
 my_dpi = 200
 
-fig = plt.figure(figsize=(float(image.size[0]) / my_dpi, float(image.size[1]) / my_dpi), dpi=my_dpi)
-ax = fig.add_subplot(111)
+grid = plt.figure(figsize=(float(background.size[0]) / my_dpi, float(background.size[1]) / my_dpi), dpi=my_dpi)
+ax = grid.add_subplot(111)
 
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+grid.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
 myInterval = 133
-loc = plticker.MultipleLocator(base=myInterval)
-ax.xaxis.set_major_locator(loc)
-ax.yaxis.set_major_locator(loc)
+a = plticker.MultipleLocator(base=myInterval)
+ax.xaxis.set_major_locator(a)
+ax.yaxis.set_major_locator(a)
 
 ax.grid(which='major', axis='both', linestyle='-', color='w')
 
-ax.imshow(image)
+ax.imshow(background)
 
-fig.savefig('ForestMapGrid.png', dpi=my_dpi)
+grid.savefig('ForestMapGrid.png', dpi=my_dpi)
