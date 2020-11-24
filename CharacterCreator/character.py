bfc691ccad11d862c16d56b8402ee51bc6af3a40
stephenjonglee <stephenjonglee@csu.fullerton.edu>
2020-11-23 19:05:49 -0800

    updated character folder

--- CharacterCreator/character.py
+++ CharacterCreator/character.py
@@ -1,4 +1,5 @@
 from flask import *
+from CharacterCreator_v1_3 import *
 import sqlite3, hashlib, os
 from werkzeug.utils import secure_filename
 
@@ -17,17 +18,24 @@ def character_form():
         # Parse form data    
         c_name = request.form['name']
         c_race = request.form['race']
+        c_language = request.form['language']
+        c_trait = request.form['trait']
         c_class = request.form['class']
+        c_proficiency = request.form.getlist('proficiency')
         c_background = request.form['background']
         c_alignment = request.form['alignment']
 
+        # Getting character description from form
+        c_gender = request.form['gender']
         c_age = request.form['age']
         c_hgt = request.form['hgt']
         c_wgt = request.form['wgt']
         c_eye = request.form['eye']
         c_skin = request.form['skin']
         c_hair = request.form['hair']
+        c_faith = request.form['faith']
 
+        # Getting character stats from form
         c_str = request.form['str']
         c_dex = request.form['dex']
         c_con = request.form['con']
@@ -35,35 +43,58 @@ def character_form():
         c_wis = request.form['wis']
         c_cha = request.form['cha']
 
-        # with sqlite3.connect('quiz.db') as con:
-        #     try:
-        #         cur = con.cursor()
+        # create stat block
+        stat_block = [c_str, c_dex, c_con, c_int, c_wis, c_cha]
 
-        #         # update last row
-        #         sql = ''' UPDATE quiz 
-        #         SET qArray = qArray || ?, 
-        #         optionArray = optionArray || ? 
-        #         WHERE quizId = (SELECT MAX(quizId) FROM quiz)'''
-        #         cur.execute(sql, (question, optArray))
-        #         con.commit()
-        #     except:
-        #         con.rollback()
-        # con.close()
+        # get ids
+        id_race = getRaceID(c_race)
+        id_class = getClassID(c_class)
+
+        # getting other attributes based on race/class
+        c_spd = getSpeed(id_race)
+        c_lang = getLangKnown(id_race)
+        c_start_prof = getStartingProf(id_race, id_class)
+        c_hit_die = getHitDieType(id_class)
+        c_prof_bonus = getProfBonus(1)
+
+        # # create empty character in database
+        # dummy_user = 1
+        # dummy_char = 1
+        # createDefaultChar(dummy_user, dummy_char)
+
+        # update input values into character database
+        # updateName(dummy_char, c_name)
+        # updateCharLvl(dummy_char, 1)
+        # updateBackground(dummy_char, c_background)
+        # updateAlignment(dummy_char, c_alignment)
+        # updateRace(c_race, dummy_char)
+        # updateClass(c_class, dummy_char)
+        # updateStatBlock(dummy_char, id_race, stat_block)
+        # convertStatsToMods(stat_block)
+        # updateSavingThrows(dummy_char, stat_block)
+        # updateSkills(dummy_char, stat_block)
+        # updateProfBonusSkills(c_proficiency, dummy_char)
+        # updateProfBonusSavingThrows(dummy_char, id_class)
+        # updateProfBonus(dummy_char, c_prof_bonus)
+
+        # delete character
+        #deleteChar(dummy_char)
+
 
         # redirct to test post
-        return render_template("test_post.html", c_name=c_name, c_race=c_race, c_class=c_class, c_background=c_background, c_alignment=c_alignment, 
-        c_age=c_age, c_hgt=c_hgt, c_wgt=c_wgt, c_eye=c_eye, c_skin=c_skin, c_hair=c_hair, c_str=c_str, c_dex=c_dex, c_con=c_con,
-        c_int=c_int, c_wis=c_wis, c_cha=c_cha)    
+        return render_template("character_sheet.html", c_name=c_name, c_race=c_race, c_class=c_class, c_background=c_background, c_alignment=c_alignment, 
+        c_gender=c_gender, c_age=c_age, c_hgt=c_hgt, c_wgt=c_wgt, c_eye=c_eye, c_skin=c_skin, c_hair=c_hair, c_faith=c_faith, c_str=c_str, c_dex=c_dex, c_con=c_con,
+        c_int=c_int, c_wis=c_wis, c_cha=c_cha, c_spd=c_spd, c_lang=c_lang, c_start_prof=c_start_prof, c_proficiency=c_proficiency, c_hit_die=c_hit_die)
     
     else:
         # display the character form
         return render_template("character_form.html")
 
 # route for test post
-@app.route('/test_post.html', methods = ['GET', 'POST'])
-def test_post():
+@app.route('/character_sheet.html', methods = ['GET', 'POST'])
+def character_sheet():
     # display the test post
-    return render_template("test_post.html")
+    return render_template("character_sheet.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- CharacterCreator/static/js/character_form.js
+++ CharacterCreator/static/js/character_form.js
@@ -1,14 +1,141 @@
-$(document).ready(function () {
-    $("#type").change(function () {
-        var val = $(this).val();
-        if (val == "item1") {
-            $("#size").html("<option value='test'>item1: test 1</option><option value='test2'>item1: test 2</option>");
-        } else if (val == "item2") {
-            $("#size").html("<option value='test'>item2: test 1</option><option value='test2'>item2: test 2</option>");
-        } else if (val == "item3") {
-            $("#size").html("<option value='test'>item3: test 1</option><option value='test2'>item3: test 2</option>");
-        } else if (val == "item0") {
-            $("#size").html("<option value=''>--select one--</option>");
+$(document).ready(function() {
+    // Display Additional Language or Trait Depending on Race Selected
+    // $('#id_race').change(function() {
+    //     var limit = 0;
+    //     var race = $( "#id_race" ).val();
+
+    //     if (race == "Half-Elf") {
+    //         $("#default_l").hide();
+    //         $("#elf").hide();
+    //     }
+
+    //     else if (race == "Human") {
+    //         $('.opt_l').prop('disabled', false);
+    //         $('.default_l').prop('disabled', true);
+
+    //         $('.opt_t').prop('disabled', true);
+    //         $('#id_trait').prop('selectedIndex', 0);
+    //     }
+
+    //     else if (race == "Dragonborn") {
+    //         $('.opt_t').prop('disabled', false);
+    //         $('.opt_t').removeAttr("disabled");
+    //         $('.default_t').prop('disabled', true);
+
+    //         $('.opt_l').prop('disabled', true);
+    //         $('#id_lang').prop('selectedIndex', 0);
+    //     }
+
+    //     else {
+    //         $('.opt_l').prop('disabled', true);
+    //         $('.opt_t').prop('disabled', true);
+    //         $('#id_trait').prop('selectedIndex', 0);
+    //         $('#id_lang').prop('selectedIndex', 0);
+    //     }
+    // });
+
+    // Display Additional Proficiency Depending on Class Selected
+    // Number of checked boxes allowed is also displayed
+    $('#id_class').change(function() {
+        var var_class = $( "#id_class" ).val();
+        $('input.chk').prop('checked', false);
+        $("input.chk").prop("disabled", true);
+
+        if (var_class == "Barbarian") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl1").prop("disabled", false);
+        }
+
+        else if (var_class == "Bard") {
+            limit = 3;
+            $( "#number" ).html("Select 3");
+
+            $("input.cl2").prop("disabled", false);
+        }
+
+        else if (var_class == "Cleric") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl3").prop("disabled", false);
+        }
+
+        else if (var_class == "Druid") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl4").prop("disabled", false);
+        }
+
+        else if (var_class == "Fighter") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl5").prop("disabled", false);
+        }
+
+        else if (var_class == "Monk") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl6").prop("disabled", false);
+        }
+
+        else if (var_class == "Paladin") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl7").prop("disabled", false);
+        }
+
+        else if (var_class == "Ranger") {
+            limit = 3;
+            $( "#number" ).html("Select 3");
+
+            $("input.cl8").prop("disabled", false);
+        }
+
+        else if (var_class == "Rogue") {
+            limit = 4;
+            $( "#number" ).html("Select 4");
+
+            $("input.cl9").prop("disabled", false);
+        }
+
+        else if (var_class == "Sorcerer") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl10").prop("disabled", false);
+        }
+
+        else if (var_class == "Warlock") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl11").prop("disabled", false);
+        }
+
+        else if (var_class == "Wizard") {
+            limit = 2;
+            $( "#number" ).html("Select 2");
+
+            $("input.cl12").prop("disabled", false);
+        }
+
+        else {
+            limit = 0;
+            $( "#number" ).html("Select a Class First");
+            $("#most").prop("disabled", true);
         }
     });
-});
+
+    // limit the number of checked boxes
+    $('input.chk').on('change', function(evt) {
+        if($(this).siblings(':checked').length >= limit) {
+            this.checked = false;
+        }
+     });
+  });
